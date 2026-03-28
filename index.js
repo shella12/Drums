0360f44ab4c1ac6da005ea15ce8ced235feea6e8
shella12 <ayeshaarshad4567@gmail.com>
2026-03-28 16:59:59 +0500

    All code

---
+++ index.js
@@ -0,0 +1,49 @@
+const keys = document.body.querySelectorAll(".key")
+let audio;
+console.log(keys)
+keys.forEach(key => {
+  key.addEventListener("click", () => {
+        console.log(key.firstElementChild.innerText)
+        const play = key.firstElementChild.innerText;
+        toggleClassName(play)
+        switch(play){
+            case "A":
+                audio = new Audio("sounds/clap.wav")
+                audio.play()
+            case "S":
+                audio = new Audio("sounds/hihat.wav")
+                audio.play()
+            case "D":
+                audio = new Audio("sounds/kick.wav")
+                audio.play()
+            case "F":
+                audio = new Audio("sounds/openhat.wav")
+                audio.play()
+            case "G":
+                audio = new Audio("sounds/boom.wav")
+                audio.play()
+            case "H":
+                audio = new Audio("sounds/ride.wav")
+                audio.play()
+            case "J":
+                audio = new Audio("sounds/snare.wav")
+                audio.play()
+            case "K":
+                audio = new Audio("sounds/tom.wav")
+                audio.play()
+            case "L":
+                audio = new Audio("sounds/tink.wav")
+                audio.play()
+        }
+    })
+});
+
+const delay =(ms) => new Promise(resolve => setTimeout(resolve, ms))
+
+const toggleClassName = async (key) => {
+   
+        const pressedKey = document.getElementById(key);
+        pressedKey.classList.add("playing");
+        await delay(100);
+        pressedKey.classList.remove("playing");
+};
